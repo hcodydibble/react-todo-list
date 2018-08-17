0c425c6f7d104c0d0868e25034b986d043f92b76
Hewlette Cody Dibble <hcodydibble@gmail.com>
2018-08-17 12:31:04 -0700

    added directions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ class TodoApp extends Component {
     return (
       <div className="App">
         <DayScheduleContainer />
+
+        <h3>Navigation: </h3>
+        <p>SHIFT+SCROLL WHEEL: Scroll forwards or backwards through the hours in the day.</p>
+        <p>ALT+SCROLL WHEEL: Zoom in or out at a manageable speed.</p>
+        <p>CTRL+SCROLL WHEEL: Zoom in or out at a crazy speed.</p>
+        <p>Change the start and end time by clicking on the event you wish to change and then dragging to the desired time. Extend the time by dragging from either edge.</p>
       </div>
     );
   }
